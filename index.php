366cbb66df07ad4e2ae2eaa508d22455b0ef7a43
Victor Kurauchi <victorkurauchi@gmail.com>
2013-07-24 20:49:13 -0300

    Initializing
    personal site built with PureCSS

---
+++ index.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Out of my mind ~ Victor Kurauchi</title>
+
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.1/base-min.css">
+    <link rel="stylesheet" href="css/pure-min.css">
+    <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/auxiliar.css">
+
+    <!-- Font -->
+    <script src="http://use.typekit.net/ajf8ggy.js"></script>
+    <script>
+        try { Typekit.load(); } catch (e) {}
+    </script>
+
+    <!-- Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-41480445-1', 'purecss.io');
+        ga('send', 'pageview');
+    </script>
+</head>
+<body>
+
+<div class="pure-g-r" id="layout">
+    <div class="sidebar pure-u">
+        <header class="header pure-u-1">
+            <hgroup>
+                <h1 class="brand-title" id="link-return">victor kurauchi</h1>
+                <h2 class="brand-tagline">web development.</h2>
+            </hgroup>
+
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a class="pure-button" id="display-blog" href="http://outtofmymindd.tumblr.com" target="_blank">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="pure-button" href="http://yuilibrary.com">Projetos</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+
+    <div id="initial-content" class="content pure-u-1">
+        <div class="posts pure-g">
+            <section class="post pure-u-1 main-content">
+
+                <div class="header-content">    
+                    <h3>Sometimes what you call everything, i just call nothing.</h3>
+                </div>
+
+                    <h4>Pessoal</h4>
+                    <p>- 19 anos, falso escritor, curioso, sonhador. Mais uma daquelas crianças que tentam agarrar o mundo com a mão. <br/>
+                    - Formado em Análise e Desenvolvimento de Sistemas pela <a href="http://fiap.com.br" target="_blank">FIAP.</a> </p>
+                    
+                    <p>Ingressei na área de desenvolvimento web em 2011 ainda com dúvidas em relação ao que realmente gostaria de fazer, estive envolvido em projetos com responsabilidades no back-end com linguagens como PHP e Java. <br/>
+                        Vivenciando diversas etapas nos processos de desenvolvimento de sites, sistemas e hotsites pude agregar diversos conteúdos para aplicar em determinados momentos e necessidades. <br/>
+                        Após muita curiosidade e vasculhando pela imensa web, foi surgindo o interesse pelo desenvolvimento front-end, porém não abandonei o server-side.</p>
+
+                <div class="clearfix"></div>
+
+                <div id="social-networks">
+                    <a class="social-network twitter" href="http://twitter.com/victorkurauchi" target="_blank"></a>
+                    <a class="social-network tumblr" href="http://outtofmymind.tumblr.com" target="_blank"></a>
+                    <a class="social-network facebook" href="http://facebook.com/victorkurauchi" target="_blank"></a>
+                    <a class="social-network linkedin" href="http://www.linkedin.com/pub/victor-moriyuki-kurauchi/29/42/86b" target="_blank"></a>
+                    <a class="social-network pinterest" href="http://pinterest.com/victorkurauchi/" target="_blank"></a>
+                    <a class="social-network googleplus" href="https://plus.google.com/u/0/116673293861066060587/posts" target="_blank"></a>
+                    <a class="social-network email" href="mailto:victorkurauchi@gmail.com" target="_blank"></a>
+                    <a class="social-network instagram" href="http://instagram.com/victorkurauchi" target="_blank"></a>
+                </div> 
+            </section>
+        </div>
+    </div>    
+
+    <div id="blog-content" class="content pure-u-1" >
+        <!-- A wrapper for all the blog posts -->
+        <div class="posts pure-g">
+            <h1 class="content-subhead pure-u-1">Pinned Post</h1>
+
+            <!-- A single blog post -->
+            <section class="post pure-u-1">
+                <header class="post-header">
+                    <img class="post-avatar"
+                         alt="Tilo Mitra's avatar" src="/img/common/tilo-avatar.png"
+                         height="48" width="48">
+
+                    <h2 class="post-title pure-u-1">Introducing Pure</h2>
+
+                    <p class="post-meta pure-u-1">
+                        By <a href="#" class="post-author">Tilo Mitra</a> under <a class="post-category post-category-design" href="#">CSS</a> <a class="post-category post-category-pure" href="#">Pure</a>
+                    </p>
+                </header>
+
+                <div class="post-description pure-u-1">
+                    <p>
+                        Yesterday at CSSConf, we launched Pure – a new CSS library. Phew! Here are the <a href="https://speakerdeck.com/tilomitra/pure-bliss">slides from the presentation</a>. Although it looks pretty minimalist, we’ve been working on Pure for several months. After many iterations, we have released Pure as a set of small, responsive, CSS modules that you can use in every web project.
+                    </p>
+                </div>
+            </section>
+        </div>
+
+        <div class="posts pure-g">
+            <h1 class="pure-u-1 content-subhead">Recent Posts</h1>
+
+            <section class="post pure-u-1">
+                <header class="post-header">
+                    <img class="post-avatar"
+                         alt="Eric Ferraiuolo's avatar" src="/img/common/ericf-avatar.png"
+                         height="48" width="48">
+
+                    <h2 class="post-title pure-u-1">Everything You Need to Know About Grunt</h2>
+
+                    <p class="post-meta pure-u-1">
+                        By <a class="post-author" href="#">Eric Ferraiuolo</a> under <a class="post-category post-category-js" href="#">JavaScript</a>
+                    </p>
+                </header>
+
+                <div class="post-description pure-u-1">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                </div>
+            </section>
+
+            <section class="post pure-u-1">
+                <header class="post-header">
+                    <img class="post-avatar" alt="Reid Burke's avatar"
+                         src="/img/common/reid-avatar.png"
+                         height="48" width="48">
+
+                    <h2 class="post-title pure-u-1">Photos from CSSConf and JSConf</h2>
+
+                    <p class="post-meta pure-u-1">
+                        By <a class="post-author" href="#">Reid Burke</a> under <a class="post-category" href="#">Uncategorized</a>
+                    </p>
+                </header>
+
+                <div class="post-description pure-u-1">
+                    <div class="post-images pure-g-r">
+                        <div class="pure-u-1-2">
+                            <a href="http://www.flickr.com/photos/uberlife/8915936174/">
+                                <img alt="Photo of someone working poolside at a resort"
+                                     src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg">
+                            </a>
+
+                            <div class="post-image-meta pure-u-1">
+                                <h3>CSSConf Photos</h3>
+                            </div>
+                        </div>
+
+                        <div class="pure-u-1-2">
+                            <a href="http://www.flickr.com/photos/uberlife/8907351301/">
+                                <img alt="Photo of the sunset on the beach"
+                                     src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg">
+                            </a>
+
+                            <div class="post-image-meta pure-u-1">
+                                <h3>JSConf Photos</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="post pure-u-1">
+                <header class="post-header">
+                    <img class="post-avatar"
+                         alt="Andrew Wooldridge's avatar" src="/img/common/andrew-avatar.png"
+                         height="48" width="48">
+
+                    <h2 class="post-title pure-u-1">YUI 3.10.2 Released</h2>
+
+                    <p class="post-meta pure-u-1">
+                        By <a class="post-author" href="#">Andrew Wooldridge</a> under <a class="post-category post-category-yui" href="#">YUI</a>
+                    </p>
+                </header>
+
+                <div class="post-description pure-u-1">
+                    <p>
+                        We are happy to announce the release of YUI 3.10.2! You can find it now on the Yahoo! CDN, download it directly, or pull it in via npm. We’ve also updated the YUI Library website with the latest documentation.
+                    </p>
+                </div>
+            </section>
+        </div>
+
+        <footer class="footer">
+            <div class="pure-menu pure-menu-horizontal pure-menu-open">
+                <ul>
+                    <li><a href="http://outtofmymindd.tumblr.com" target="_blank">Tumblr</a></li>
+                    <li><a href="http://twitter.com/yuilibrary/" target="_blank">Twitter</a></li>
+                    <li><a href="http://github.com/victorkurauchi" target="_blank">Github</a></li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</div>
+
+<script src="http://yui.yahooapis.com/3.10.1/build/yui/yui-min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+
+$(document).ready(function() {
+    document.getElementById('blog-content').style.display = "none";
+    document.getElementById('link-return').style.cursor = "pointer";
+    
+});
+
+YUI().use('node-base', 'node-event-delegate', function (Y) {
+    // This just makes sure that the href="#" attached to the <a> elements
+    // don't scroll you back up the page.
+    Y.one('.content').delegate('click', function (e) {
+        e.preventDefault();
+    }, 'a[href="#"]');
+});
+
+$(document.body).on('click', '#display-blog', function(event) {
+    document.getElementById('blog-content').style.display = "block";
+    document.getElementById('initial-content').style.display = "none";
+})
+
+$(document.body).on('click', '#link-return', function(event) {
+    document.getElementById('blog-content').style.display = "none";
+    document.getElementById('initial-content').style.display = "block";
+})
+
+</script>
+
+</body>
+</html>
